bd201f792a017e6e405667019d234bc20a549c0c
Shasa <shasa.thuo@gmail.com>
2020-04-26 11:01:29 +0300

    A string is reversed

--- reverse_string.py
+++ reverse_string.py
@@ -1,1 +1,15 @@
-#TODO, write a function in python that reverses a string 
+#TODO, write a function in python that reverses a string
+
+string_input = input("Enter word to reverse here: ")
+
+#Get the length of the string 
+length_string = len(string_input)
+
+#Process to reverse string 
+reverse_text = ''
+
+while length_string > 0:
+    reverse_text = reverse_text + string_input[length_string -1]
+    length_string = length_string -1 
+
+print("The reversed string is>> ", reverse_text)
