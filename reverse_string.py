030d7cc8edb2b73d3773713b9b1099c3960b88c0
Shasa <shasa.thuo@gmail.com>
2020-04-27 21:38:12 +0300

    Turned it into a function

--- reverse_string.py
+++ reverse_string.py
@@ -1,15 +1,21 @@
-#TODO, write a function in python that reverses a string
 
-string_input = input("Enter word to reverse here: ")
+def reverse_word(text):
+    '''
+    Function takes a word and reverses it 
+    Len() gets the length of teh string
+    Loop takes the last digit in teh string, saving it into the first position in the string reversed_text
+    '''
+    text_reverse = text
+    text_lengh = len(text_reverse)
+    reversed_text = ''
 
-#Get the length of the string 
-length_string = len(string_input)
+    #Reverse the text using a while loop 
+    while text_lengh > 0:
+        reversed_text = reversed_text + text_reverse[text_lengh -1]
+        text_lengh = text_lengh -1
 
-#Process to reverse string 
-reverse_text = ''
+    return reversed_text
 
-while length_string > 0:
-    reverse_text = reverse_text + string_input[length_string -1]
-    length_string = length_string -1 
-
-print("The reversed string is>> ", reverse_text)
+#Activating the function
+print('The reversed word is') 
+print(reverse_word("asdft"))
