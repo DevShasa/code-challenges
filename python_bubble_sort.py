6ca1bd55d1292ad8999cc1cf4bf049ae81072304
Shasa <shasa.thuo@gmail.com>
2020-05-13 18:19:26 +0300

    Big bug solved

--- python_bubble_sort.py
+++ python_bubble_sort.py
@@ -1,5 +1,5 @@
-import copy
 import random
+import copy
 #Demonstration of bubble sort to sort numbers 
 '''
 First begin by inputting numbers into a list 
@@ -7,26 +7,21 @@ The sort numbers in the list using a for loop
 Nested if statements inside the for loop will compare two numbers and switch them in order to sort the list 
 '''
 #Activate the list 
-sort_list = []
+sort_list =[]
+
 
 #Create ten random numbers and save them into a list
 for number in range(0,11):
     random_number = random.randint(1,100)
-    sort_list.append(random)
+    sort_list.append(random_number)
   
-#Copy a fresh unchanged copy of the list so that it can be sorted low to high and high to low 
-new_list = copy.deepcopy(sort_list)
-
-
-# #Input ten numbers to sort into a list 
-# for input_count in range(1,11):
-#     prompt = "Please enter the " +str(input_count)+ " Number: "
-#     number_input = input(prompt)
-#     sort_list.append(number_input)
 
 #Test that code to input stuff is working
 print("The unsorted list is") 
 print(sort_list)
+
+#Copy the list into a new list for the sort high to low loop
+new_list = copy.deepcopy(sort_list)
 
 #Sort the list from lowest to highest using bubble sort 
 for outside_count in range(0, len(sort_list)+1):
@@ -46,8 +41,13 @@ for count2 in range(0,len(new_list)+1):
             new_list[count3] = new_list[count3+1]
             new_list[count3+1] = temp
 
+
+
 #Lets see if it works shall we 😰
 print("The list sorted from lowest to highest")
 print(sort_list)
+
+
 print('The list sorted from highest to lowest is')
 print(new_list)
+
