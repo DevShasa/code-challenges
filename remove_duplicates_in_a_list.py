6f1dc4d0257584247a0150103d0bdc81e175a9f3
Shasa <shasa.thuo@gmail.com>
2020-05-16 13:13:36 +0300

    Remove duplicates from a list

--- remove_duplicates_in_a_list.py
+++ remove_duplicates_in_a_list.py
@@ -1,1 +1,15 @@
+#In this here file we get to see a simple way of removing duplicates from a list 
 
+#First step, create a list with some duplicate values in it 
+list_with_duplicates = ["a","f","a","h","w","h","x","x","t","t","n","n","l","l","l","a","f","f","q","t","h"]
+
+#Create a new list that will hold the sorted list withoud duplicates
+new_sorted_list = []
+
+#Go through the list checking for duplicates and eliminate duplicates
+for list_item in list_with_duplicates:
+    if list_item not in list_with_duplicates:
+        new_sorted_list.append(list_item)
+
+print('the unsorted list is')
+print(new_sorted_list)
