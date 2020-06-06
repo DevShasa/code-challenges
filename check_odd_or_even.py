75dfa099e8710b9a760c56baac6bd58bb061584b
Shasa <shasa.thuo@gmail.com>
2020-06-06 20:55:28 +0300

    Accept and validate user inout

--- check_odd_or_even.py
+++ check_odd_or_even.py
@@ -1,1 +1,14 @@
 #This code checks if a number is odd or even
+#Accept user input and validate it to ensure that it is a number
+while True:
+    try:
+        usr_input = input("Please enter a number: ")
+        int_usr_inpur = int(usr_input)
+        #If everything goes well break the loop 
+        break
+    except ValueError:
+        #If the user enters something other than a number, perform these statements 
+        print("Please only enter a number")
+        continue
+
+#This is where we check whether the number is odd or even 
