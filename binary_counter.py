e4412fbb2e33f95bd1cd3723b4f9bec88965d779
Shasa <shasa.thuo@gmail.com>
2020-07-01 08:34:12 +0300

    Code is complete and it works

--- binary_counter.py
+++ binary_counter.py
@@ -1,7 +1,7 @@
 #Code in here counts the number of ones in the binary representation of a number 
 #For instance, code will convert two into 10 and print out that it has one '1'
 
-#Step one, take in a number from the user and make sure that the number is an integer
+#STEP ONE, take in a number from the user and make sure that the number is an integer
 while True:
     user_number = input('Please enter number: ')
     try:
@@ -12,3 +12,19 @@ while True:
         #Jump to the top of the loop to try again
         continue
 
+#STEP TWO, convert the number to binary using inbuilt pythn function 
+usr_number_bin = bin(user_number_int)
+
+#STEP THREE, count the number of ones in the binary number using a for loop
+#Here we convert to a string so that we can compare each digit in the string  
+usr_string = str(usr_number_bin)
+count = 0
+for counter in usr_string:
+    #If a digit in teh string is 1 increase counter by one 
+    if counter == '1':
+        count = count + 1
+
+#STEP FOUR, print out the result 
+print('The interger number you entered is', user_number_int)
+print('Its binary representation is', usr_number_bin)
+print('The number of ones in the binary representation is', count)
