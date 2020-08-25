54406e209907fb96ab470442b4d51324d4daa9ab
Shasa <shasa.thuo@gmail.com>
2020-08-25 21:22:59 +0300

    Added a python file containing code that counts the occurence of characters in a string

---
+++ occurences_in_a_string.py
@@ -0,0 +1,16 @@
+# Given a string s and a character c..
+# Return the number of occurences of C in s
+# For instance numChars('chameleon', 'e')
+
+def count_characters(string, character):
+    c = 0
+    for x in string:
+        if x == character:
+            c = c + 1
+    return c
+
+# Activating the function
+
+
+occurences = count_characters('chameleon', 'e')
+print(occurences)
