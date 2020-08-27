51fd9e908d398da86f683a2cc3edb7f0f3cb7ada
Shasa <shasa.thuo@gmail.com>
2020-08-27 21:48:41 +0300

    Made changes to adhere to pylama linter

--- python_bubble_sort.py
+++ python_bubble_sort.py
@@ -1,50 +1,50 @@
 import random
 import copy
-#Demonstration of bubble sort to sort numbers 
+# Demonstration of bubble sort to sort numbers
 '''
-First begin by inputting numbers into a list 
+First begin by inputting numbers into a list
 The sort numbers in the list using a for loop
-Nested if statements inside the for loop will compare two numbers and switch them in order to sort the list 
+Nested if statements inside the for loop will compare two numbers...
+... and switch them in order to sort the list
 '''
-#Activate the list 
-sort_list =[]
+# Activate the list
+sort_list = []
 
 
-#Create ten random numbers and save them into a list
-for number in range(0,11):
-    random_number = random.randint(1,100)
+# Create ten random numbers and save them into a list
+for number in range(0, 11):
+    random_number = random.randint(1, 100)
     sort_list.append(random_number)
-  
 
-#Test that code to input stuff is working
-print("The unsorted list is") 
+
+# Test that code to input stuff is working
+print("The unsorted list is")
 print(sort_list)
 
-#Copy the list into a new list for the sort high to low loop
+# Copy the list into a new list for the sort high to low loop
 new_list = copy.deepcopy(sort_list)
 
-#Sort the list from lowest to highest using bubble sort 
+# Sort the list from lowest to highest using bubble sort
 for outside_count in range(0, len(sort_list)+1):
-    #Create a flag that terminates the loop when there are no more comparisons to make,
-    #Use of flag makes it a bit more efficient 
+    # Create a flag that terminates the loop when...
+    # ...there are no more comparisons to make,
+    # Use of flag makes it a bit more efficient
     flag = False
     for inside_count in range(0, len(sort_list)-1-outside_count):
         if sort_list[inside_count] > sort_list[inside_count+1]:
-            #If the value on the left is bigger than the value on the right, switch them 
             temp = sort_list[inside_count]
             sort_list[inside_count] = sort_list[inside_count+1]
             sort_list[inside_count+1] = temp
             flag = True
-    #If there are no more comparisons to make break the loop 
+    # If there are no more comparisons to make break the loop
     if flag == False:
         break
 
-#Sort the list from highest to lowest, basicaly the same as above just change the comparison operator
-for count2 in range(0,len(new_list)+1):
+# Sort the list from highest to lowest
+for count2 in range(0, len(new_list)+1):
     flag2 = False
     for count3 in range(len(new_list)-1-count2):
         if new_list[count3] < new_list[count3+1]:
-            #If left digit is smaller than right digit, switch them 
             temp = new_list[count3]
             new_list[count3] = new_list[count3+1]
             new_list[count3+1] = temp
@@ -53,12 +53,10 @@ for count2 in range(0,len(new_list)+1):
         break
 
 
-
-#Lets see if it works shall we 😰
+# Lets see if it works shall we 😰
 print("The list sorted from lowest to highest")
 print(sort_list)
 
 
 print('The list sorted from highest to lowest is')
 print(new_list)
-
--- random_string.py
+++ random_string.py
@@ -1,35 +1,38 @@
-#Code to generate list of random string 
-#First create a list with the alphabet
-#Then generate random string 
-#Then save the strings into a file 
+# Code to generate list of random string
+# First create a list with the alphabet
+# Then generate random string
+# Then save the strings into a file
 '''
-What is the point of this? 
-Useful perhaps for generating random keys for a database or for creating security keys
-''' 
+What is the point of this?
+Useful perhaps for generating random keys for...
+... a database or for creating security keys
+'''
+
 import random
 
-#STEP ONE: create a list with letters 
+# STEP ONE: create a list with letters
 letters = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
 
-#STEP TWO, generate random string and save them in a file
-#Create an object that enables us to save in a file 
-output_file = open('random_words.txt','w')
+# STEP TWO, generate random string and save them in a file
+# Create an object that enables us to save in a file
+output_file = open('random_words.txt', 'w')
 
-#Outer loop iterates a hundred times, saving a random string in the file 
-for count in range(0,100):
-    #Clear the variable 
+# Outer loop iterates a hundred times, saving a random string in the file
+for count in range(0, 100):
+    # Clear the variable
     rw = ''
-    #Inner loob builds the word, the number of iterations is the lenght of the word
-    for count in range(0,20):
-        #Pick a random letter 
-        random_letter = random.randint(0,len(letters)-1)
-        #Buld the string  
-        rw =  rw + letters[random_letter]
-    #Print the string 
+    # Inner loop builds the word,
+    # the number of iterations is the lenght of the word
+    for count in range(0, 20):
+        # Pick a random letter
+        random_letter = random.randint(0, len(letters)-1)
+        # Buld the string
+        rw = rw + letters[random_letter]
+    # Print the string
     print(rw)
-    #Save the string in a file
-    file_line = rw +'\n'
-    output_file.write(file_line) 
+    # Save the string in a file
+    file_line = rw + '\n'
+    output_file.write(file_line)
 
-#Close the file 
+# Close the file
 output_file.close()
