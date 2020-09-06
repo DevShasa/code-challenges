282a13a4731a8c59a63d44cd1baec40b973032c7
Shasa <shasa.thuo@gmail.com>
2020-09-06 20:25:03 +0300

    Pylama formating

--- Casidoo-interview-questions/question1.py
+++ Casidoo-interview-questions/question1.py
@@ -1,7 +1,7 @@
 # Given an array of numbers that represent stock prices
 # Where each number is the price for a certain day
 # Find two days when you should buy and sell your stock for...
-# ...the highest profit 
+# ...the highest profit
 # For instance stockBuySell([110, 180, 260, 40, 310, 535, 695])
 # The code should produce "buy on day 4 sell on day 7"
 
@@ -11,7 +11,7 @@ def sort_buy_sell(array):
     sellcounter = 0
     counter = 0
     sell = 0
-    buy =  array[0]
+    buy = array[0]
     for count in array:
         counter = counter + 1
         if sell < count:
@@ -23,6 +23,8 @@ def sort_buy_sell(array):
     print("buy", buy, "sell", sell)
     print("Buy on day", buycounter, "sell on", sellcounter)
 
-#Activating the function
+# Activating the function
+
+
 array = [110, 180, 260, 40, 310, 535, 695]
 sort_buy_sell(array)
--- smallest_and_largest_number.py
+++ smallest_and_largest_number.py
@@ -1,24 +1,24 @@
-#This code is for the purpose of finding the largest and smallest number in a list 
+# This code is for the purpose of finding the largest and smallest number in a list 
 
-#STEP ONE, use python random function to populate the list with ten random numbers 
+# STEP ONE, use python random function to populate the list with ten random numbers 
 import random
 number_list = []
 #Fill the list with ten random numbers between 1 and 100
-for count in range(1,10+1):
+for count in range(1, 10+1):
     random_number = random.randint(1,100)
     number_list.append(random_number)
-#Print the list 
+# Print the list
 print("The ten random numbers are")
 print(number_list)
 
-#STEP TWO find the smallest number in the list
+# STEP TWO find the smallest number in the list
 smallest_number = number_list[0]
 for count1 in number_list:
     if smallest_number > count1:
         smallest_number = count1
 print('The smallest number is', smallest_number)
 
-#STEP THREE find the largest number in the list 
+# STEP THREE find the largest number in the list
 largest_number = 0
 for count2 in number_list:
     if largest_number < count2:
