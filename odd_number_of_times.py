7da6cb136562f553e65beb5494a5c3ca84012bd2
Shasa <shasa.thuo@gmail.com>
2020-10-01 22:27:13 +0300

    refactored and made compliant with pep8

--- odd_number_of_times.py
+++ odd_number_of_times.py
@@ -1,23 +1,24 @@
 # In an array of numbers
-# Identify the number that appears an odd number of times 
-l = [2, 3, 5, 4, 5, 2, 4, 3, 5, 2, 4, 4, 2]
+# Identify the number that appears an odd number of times
+def count_odd(arrayOfNumbers):
+    # get length of array
+    a_size = len(arrayOfNumbers)
 
-# get length of array 
-a_size = len(l)
-
-# Outer for loop 
-for i in range(0, a_size):
-    count = 0
-    for x in range(0, a_size):
-        # Compare the values 
-        if l[i] == l[x]:
-            # Increment counter
-            count = count + 1
-    # Check if the number of appearances is odd using a modulo operator 
-    if (count % 2) != 0:
-        # If the result is not equal to zero, the number is odd
-        print('The number that appears an odd number of times is', l[i])
-        break
-
+    # Outer for loop
+    for i in range(0, a_size):
+        count = 0
+        for x in range(0, a_size):
+            # Compare the values
+            if arrayOfNumbers[i] == arrayOfNumbers[x]:
+                # Increment counter
+                count = count + 1
+        # Check if the number of appearances is odd
+        if (count % 2) != 0:
+            # If the result is not equal to zero, the number is odd
+            print(arrayOfNumbers[i], 'appears an odd number of times')
+            break
 
 
+# Activating the function
+numbers = [2, 3, 5, 4, 5, 2, 4, 3, 5, 2, 4, 4, 2]
+count_odd(numbers)
