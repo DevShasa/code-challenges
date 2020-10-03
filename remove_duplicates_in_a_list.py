f5e45956e77473b4ceef7caf1fb1c1a04d962b65
Shasa <shasa.thuo@gmai.com>
2020-10-03 21:59:27 +0300

    changes made

--- remove_duplicates_in_a_list.py
+++ remove_duplicates_in_a_list.py
@@ -3,15 +3,16 @@
 # First step, create a list with some duplicate values in it
 list_with_duplicates = ["a","f","a","h","w","h","x","x","t","t","n","n","l","l","l","a","f","f","q","t","h"]
 
-# Create a new list that will hold the sorted list withoud duplicates
-new_sorted_list = []
-# Go through the list checking for duplicates and eliminate duplicates
-for list_item in list_with_duplicates:
-    if list_item not in new_sorted_list:
-        new_sorted_list.append(list_item)
+def removeDuplicates(duplicateList):
+    # Create a new list that will hold the sorted list withoud duplicates
+    new_sorted_list = []
+    # Go through the list checking for duplicates and eliminate duplicates
+    for list_item in duplicateList:
+        if list_item not in new_sorted_list:
+            new_sorted_list.append(list_item)
+    return new_sorted_list
 
 
-print('the unsorted list is')
-print(list_with_duplicates)
-print('The sorted list is',)
-print(new_sorted_list)
+# Activating the function 
+result = removeDuplicates(list_with_duplicates)
+print(result)
