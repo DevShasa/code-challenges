b3845801136812d1209e7491bbafa6e29fba8688
Shasa <shasa.thuo@gmail.com>
2020-10-29 22:22:45 +0300

    File contains code to sort an array of numbers using the number of ones in their respective binary representations

---
+++ sort_by_binary.py
@@ -0,0 +1,12 @@
+# Given an integer array arr, sort the integers in arr... 
+# in ascending order by the number of 1’s in their binary... 
+# representation and return the sorted array.
+#............EXAMPLE.........
+# $ sortBits([0,1,2,3,4,5,6,7,8])
+#...result----->[0,1,2,4,8,3,5,6,7]
+def countOnes(binary_number):
+    '''
+    This function converts a number into its binary representation
+    Thefunction then counts the number of ones and returns that number
+    '''
+    pass
