ed4e4a8a4842a254ef748b50ac0f9bf6011aadd8
Shasa <shasa.thuo@gmail.com>
2020-11-03 07:53:49 +0300

    it works!

--- Casidoo-interview-questions/character_jumps.py
+++ Casidoo-interview-questions/character_jumps.py
@@ -1,28 +1,28 @@
-'''
-You have a character who jumps forward n number of units at a time,
-and an array representing the road in front of them (where 0 is a 
-flat piece of road, and 1 is an obstacle). Return true or false if 
-your character can jump to the end without hitting an obstacle in front of them.
-        ......EXAMPLE......
-        $ characterJump(3, [0,1,0,0,0,1,0])
-        $ true // no hits
-        ......EXAMPLE TWO.....
-        characterJump(4, [0,1,1,0,1,0,0,0,0]) 
-        $ false // hits obstacle at position 4
-'''
+def characterJumps(jumps, road_array):
+    '''
+    You have a character who jumps forward n number of units at a time,
+    and an array representing the road in front of them (where 0 is a 
+    flat piece of road, and 1 is an obstacle). Return true or false if 
+    your character can jump to the end without hitting an obstacle in front of them.
+            ......EXAMPLE......
+            $ characterJump(3, [0,1,0,0,0,1,0])
+            $ true // no hits
+            ......EXAMPLE TWO.....
+            characterJump(4, [0,1,1,0,1,0,0,0,0]) 
+            $ false // hits obstacle at position 4
+    '''
+    count = 0
+    for i in road_array:
+        if count == jumps:
+            # Get the position of i
+            if i == 0:
+                print('True, no hits')
+                return
+            elif i == 1:
+                print('False, hits obstacle at position', jumps)
+                return
+        # Increment the counter 
+        count = count + 1
 
-road = [0,1,0,0,0,1,0]
-usr_input = input('How many units will the character jump: ')
-count = 0
-for i in road:
-    # Increment the counter 
-    count = count + 1
-    if count == usr_input:
-        # Get the position of i
-        if i == 0:
-            print('True, no hits')
-            break
-        elif i == 1:
-            print('False, hits obstacle at position', usr_input)
-            break
-
+# Activating the functtion 
+characterJumps(4, [0,1,1,0,1,0,0,0,0])
