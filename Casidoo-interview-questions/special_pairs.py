cabef319c1f9934b0cb77846d67f2d7daed605c6
Shasa <shasa.thuo@gmail.com>
2020-11-21 12:09:03 +0300

    Added the prompt for the interview question

--- Casidoo-interview-questions/special_pairs.py
+++ Casidoo-interview-questions/special_pairs.py
@@ -1,13 +1,26 @@
+'''
+Given an array of integers arr,
+a pair (n,m) is called “special” if arr[n] == arr[m]
+Return the number of special pairs.
+......EXAMPLE
+$ specialPairs([1,2,3,1,1,3])
+$ 4 // (0,3), (0,4), (3,4), (2,5)
+'''
 from collections import defaultdict
 
-def special_pairs(xs):
+
+def count_pairs(xs):
     seen = defaultdict(int)
     count = 0
     for x in xs:
-        count  =  count + seen[x]
+        # Everytime the value of a key increments...
+        # ...Count increases
+        count = count + seen[x]
         seen[x] = seen[x] + 1
     return count
 
 
-def test_example():
-    assert special_pairs([1, 2, 3, 1, 1, 3]) == 4
+# Activating the function
+array_of_integers = [1, 2, 3, 1, 1, 3]
+
+print(count_pairs(array_of_integers))
