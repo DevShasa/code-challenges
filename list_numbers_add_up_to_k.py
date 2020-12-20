04fe54d28dd6091b81c905b05e1dd10055f2d2a5
DevShasa <shasa.thuo@gmail.com>
2020-12-20 19:45:03 +0300

    Made improovement to the code to take care of edge cases

--- list_numbers_add_up_to_k.py
+++ list_numbers_add_up_to_k.py
@@ -8,12 +8,14 @@ given [10, 15, 3, 7]
 k = 17
 return true scince 10 + 7 is 17
 '''
-def add_up_true(l_of_numbers, k):
-    for x in range(0, len(l_of_numbers) -1):
-        for y in l_of_numbers:
-            z = l_of_numbers[x]
-            if y + z == k:
+def add_up_true(nums, k):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == k:
                 return True
+    
+    return False
+
 
 # Do it with tests like a gud boi 
 import unittest
@@ -25,3 +27,4 @@ class testList(unittest.TestCase):
 
 # Run the test 
 if  __name__ == "__main__":
+    unittest.main()
