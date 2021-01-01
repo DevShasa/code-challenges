bbaa3746252d4042a1c4f2b42e16bbaa141a913e
Shasa <shasa.thuo@gmail.com>
2021-01-01 12:03:21 +0300

    Made improovements to Casidoo's december 2020 interview question by using slicing

--- Casidoo-interview-questions/casidoo-28-12-2020.py
+++ Casidoo-interview-questions/casidoo-28-12-2020.py
@@ -1,13 +1,22 @@
+'''
+You’re given a string of characters that are only 2s and 0s.
+Return the index of the first occurrence of “2020” without
+using the indexOf (or similar) function, and -1 if it’s not
+found in the string.
 
+EXAMPLE
+>>> find2020(‘2220000202220020200’)
+>>> 14
+'''
 
 def find2020(string2020):
-    z = list(string2020)
-    for x in range(0, len(z)-1):
-        is_2020 = z[x]+z[x+1]+z[x+2]+z[x+3]
-        if is_2020 == '2020':
+    '''
+    Has improovements over the previous function
+    '''
+    for x in range(0, len(string2020)-1):
+        if string2020[x:x+4] == '2020':
             return x
     return -1
-
 
 # Activating the function
 print(find2020('222000020222002020'))
